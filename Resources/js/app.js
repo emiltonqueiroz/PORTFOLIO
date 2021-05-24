daa0313ec11790a5565dacf998c51e394a67cea8
emiltonqueiroz <emiltonqueiroz@gmail.com>
2021-05-24 08:01:46 +0200

    Header Section

--- Resources/js/app.js
+++ Resources/js/app.js
@@ -10,3 +10,7 @@ menuOpen.addEventListener("click", () => {
 menuClose.addEventListener("click", () => {
   overlay.classList.remove("overlayactive");
 });
+
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+tl.from(".red", {x: "50%", duration: 1.8,  delay:1.2 });
+tl.from(".yellow", {opacity:"0",x: "-30%", delay:1.2, duration:"1.7"}, {opacity:"1", x: "0%", duration:2});
